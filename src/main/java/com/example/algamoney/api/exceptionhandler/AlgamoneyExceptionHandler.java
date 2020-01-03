18ea4921e55982bb1d198972c206626ef6e1a50e
Ana <ana.clara.correa.da.silva@ibm.com>
2020-01-03 17:59:45 -0300

    Tratando Erros com ExceptionHandler

---
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -0,0 +1,57 @@
+//3.9
+package com.example.algamoney.api.exceptionhandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice //Ele observa toda a aplicação
+public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request)  {
+		//TODO Auto-generatedd method stub
+		
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale() );
+		String mensagemDesenvolvedor = ex.getCause().toString();
+		
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor ), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	public static class Erro {
+		
+		private String mensagemDesenvolvedor;
+		private String mensagemUsuario;
+		
+		public Erro(String mensagemDesenvolvedor, String mensagemUsuario) {
+			
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+			this.mensagemUsuario = mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+		
+	}
+	
+	
+	
+	
+}
