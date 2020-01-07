b724eba50b86f80317cb79ad76d9c6e5f110f95b
Ana <ana.clara.correa.da.silva@ibm.com>
2020-01-07 19:26:32 -0300

    Melhora a implementação da atualização parcial

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -3,10 +3,8 @@ package com.example.algamoney.api.resource;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
-//import com.example.algamoney.api.service.PessoaService; some functions must be moved to another class to make the code more redable
+import com.example.algamoney.api.service.PessoaService;
 
 
 /* Controlador Rest*/
@@ -72,48 +70,28 @@ public class PessoaResource {
 	}
 	
 	
+	// injetando  uma instância da classe PessoaService assim
+	@Autowired
+	private PessoaService pessoaService;
+
 	//PessoaService pService = new PessoaService();
 	
 	@PutMapping("/{codigo}")
 	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
-		Pessoa pessoaSalva = atualizarPessoa(codigo, pessoa);
+		Pessoa pessoaSalva = pessoaService.atualizarPessoa(codigo, pessoa);
 		return ResponseEntity.ok(pessoaSalva);
 	}
 	
-	public Pessoa atualizarPessoa(Long codigo, Pessoa pessoa) {
 
-		  Pessoa pessoaSalva = this.pessoaRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
-		  BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		  return this.pessoaRepository.save(pessoaSalva);
-		}
-	
-	
 	
 	@PutMapping("/{codigo}/ativo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		
-		atualizarPropriedadeAtivo2(codigo, ativo);
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 	}
 	
-	/**Atualiza apenas o campo ativo da classe pessoa*/
-	public void atualizarPropriedadeAtivo2(Long codigo, Boolean ativo) {
 
-		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
-		pessoaSalva.setAtivo(ativo);
-		pessoaRepository.save(pessoaSalva);
-		
-	}
-	
-	/**Busca a pessoa pelo ID e já verifica se o codigo é invalido (se invalido retorna 404 Not Found)*/
-	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
-		
-		Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElseThrow(() -> new EmptyResultDataAccessException(1));
-		if (pessoaSalva == null) {
-			throw new EmptyResultDataAccessException(1);
-		}
-		return pessoaSalva;
-	}
 	
 	
 }
