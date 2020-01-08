591a04f5edfa49f94d78d8f44362626fc98fa894
Ana <ana.clara.correa.da.silva@ibm.com>
2020-01-08 13:07:32 -0300

    Cria POST para a Entidade Lancamento

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -2,12 +2,20 @@ package com.example.algamoney.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 
@@ -22,6 +30,10 @@ public class LancamentoResource {
 	// injetando o acervo de lancamentos
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	//criando um disparador de evento
+	@Autowired
+	private ApplicationEventPublisher publisher; 
 
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -33,5 +45,17 @@ public class LancamentoResource {
 
 		return this.lancamentoRepository.findById(codigo).orElse(null);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody Lancamento lancamento, HttpServletResponse response) {
+
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+
+		publisher.publishEvent(new RecursoCriadoEvent(this , response, lancamentoSalvo.getCodigo()) );
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	
 	
 }
