177e3700f10cd905048522264675c81f8fe8e403
Ana <ana.clara.correa.da.silva@ibm.com>
2020-01-11 13:00:21 -0300

    Implementa Requisição DELETE pra lancamento

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.resource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -12,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -89,6 +92,14 @@ public class LancamentoResource {
 		
 		return ResponseEntity.badRequest().body(erros);
 	}
+	
+	
+	//5.7
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)//espera-se responder com o código 204
+	public void remover(@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
+	}	
 	
 	
 	
