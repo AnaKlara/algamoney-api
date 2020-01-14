7130a7244434efd08fe1225dc9341bd2612966fd
Ana <ana.clara.correa.da.silva@ibm.com>
2020-01-14 00:06:20 -0300

    Implementa segurança com OAuth 2 e Password Flow

---
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,40 @@
+package com.example.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("angular")
+			.secret("@ngul@r0")
+			.scopes("read", "write")// é possível configurar escopos diferentes para clientes diferentes
+			.authorizedGrantTypes("password")
+			.accessTokenValiditySeconds(1800);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.tokenStore(tokenStore())
+			.authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+	
+}
