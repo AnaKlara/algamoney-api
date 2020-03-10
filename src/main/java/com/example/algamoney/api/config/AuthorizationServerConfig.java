a9dbf30da070f7b75ef5eb0c413a3da0508c5730
Ana <ana.clara.correa.da.silva@ibm.com>
2020-03-10 12:59:31 -0300

    Acrescenta nome de usuário no Token JWT

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,13 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.example.algamoney.api.config.token.CustonTokenEnahncer;
 
 
 @Configuration
@@ -37,12 +43,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		
+		//7.5 : Aqui podemos acrescentar dados que queremos que sejam transportados no token
+		TokenEnhancerChain tokenEnhanceChain = new TokenEnhancerChain(); 
+		tokenEnhanceChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter() ));
+		
+		
 		endpoints
 			.tokenStore(tokenStore())
-			.accessTokenConverter(accessTokenConverter())//conversor de token, método abaixo
+			.tokenEnhancer(tokenEnhanceChain)
 			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 	}
+
+	@Bean //7.5
+	public TokenEnhancer tokenEnhancer() {
+		return new CustonTokenEnahncer();
+	}
+
 
 	@Bean // quem precisar desse método recupera através do BEan
 	public JwtAccessTokenConverter accessTokenConverter() { // setar a chave que valida o token
--- src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
+++ src/main/java/com/example/algamoney/api/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,18 +19,21 @@ import com.example.algamoney.api.repository.UsuarioRepository;
 @Service// para virar um componente
 public class AppUserDetailsService implements UserDetailsService {
 	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-
+	
+	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
-		Set<SimpleGrantedAuthority> authorities = new HashSet<>(); 
-		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao())));
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
 		return authorities;
 	}
+
 }
