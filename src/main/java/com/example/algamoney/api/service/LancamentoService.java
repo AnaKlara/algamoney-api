fe6e2eee59f147f90a3a80ea92ba1049f210e75e
Ana <ana.clara.correa.da.silva@ibm.com>
2020-03-27 14:43:06 -0300

    Gera os bytes do relatorio/cria template para relatórios

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -1,5 +1,12 @@
 package com.example.algamoney.api.service;
 
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -8,11 +15,17 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.algamoney.api.dto.LancamentoEstatisticaPessoa;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import com.example.algamoney.api.repository.PessoaRepository;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 //5.6
 
@@ -80,5 +93,25 @@ public class LancamentoService {
 			}
 			return lancamentoSalvo;
 		}	
+		
+		
+		// GERADOR DE RELATÓRIO
+		
+		public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
+			List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
+			
+			Map<String, Object> parametros = new HashMap<>();
+			parametros.put("DT_INICIO", Date.valueOf(inicio));
+			parametros.put("DT_FIM", Date.valueOf(fim));
+			parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+			
+			InputStream inputStream = this.getClass().getResourceAsStream(
+					"/relatorios/lancamentos-por-pessoa.jasper");
+			
+			JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros,
+					new JRBeanCollectionDataSource(dados));
+			
+			return JasperExportManager.exportReportToPdf(jasperPrint);
+		}
 		
 }
